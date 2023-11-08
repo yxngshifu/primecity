0894985710ead7718c40a197e863bbadd71d56c1
yxngshifu <mimadami01@gmail.com>
2023-11-09 00:51:39 +0100

    services

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,13 +2,20 @@ import { serviceSubtitle,serviceTitle } from "./data";
 import bedroomflats from '../Assets/bedroomFlats.jpg';
 import studio from '../Assets/studio.jpg';
 import suite from '../Assets/suite.jpg';
+import servicebg from '../Assets/servicebg.jpg'
 import  FadeIn from './FadeIn';
 
 const Services = () => {
    return (
-    <div id="service" className="mb-[160px] px-10 max-w-[1490px] mx-auto">
-      <FadeIn delay={0.2} direction="down">
-        <h3 className="text-5xl lg:text-[64px] font-medium text-fontBlack mb-6 text-center">
+    <div className='h-screen relative' style={{
+      backgroundImage: `url(${servicebg})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+    }}>
+    <div id="service" className="mb-[200px] px-60 max-w-[1490px] mx-auto">
+      <FadeIn delay={0.3} direction="down">
+        <h3 className="text-5xl lg:text-[40px] font-medium text-fontBlack mb-6 text-center">
           {serviceTitle}
         </h3>
       </FadeIn>
@@ -19,18 +26,19 @@ const Services = () => {
       </FadeIn>
 
       <div className="flex flex-col md:flex-row md:justify-center gap-8">
-        <FadeIn delay={0.2} direction="right">
+        <FadeIn delay={0.5} direction="right">
           <div className="flex flex-col gap-8">
             <img src={bedroomflats} alt="" />
             <img src={suite} alt="" />
           </div>
         </FadeIn>
-        <FadeIn delay={0.2} direction="left">
+        <FadeIn delay={0.5} direction="left">
           <div>
             <img src={studio} alt="" />
           </div>
         </FadeIn>
       </div>
+    </div>
     </div>
   );
 };
