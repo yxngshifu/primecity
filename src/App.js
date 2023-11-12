83ceeb9b563b8f8e39e005f8590555db85979ee2
yxngshifu <mimadami01@gmail.com>
2023-11-12 02:44:49 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Contact from './Components/Contact';
 import Hero from './Components/Hero';
@@ -7,16 +8,16 @@ import Services from './Components/Services';
 
 function App() {
   return (
-    
+    <Router>
       <div>
-        
-       <Hero />
-        <Services />
-          <Prices />
-          <Contact />
-       
+        <Routes>
+        <Route path="/" exact element={<Hero />} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/prices" element={<Prices/>} />
+        <Route path="/contact" element={<Contact/>} />
+        </Routes>
       </div>
-   
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,74 +1,57 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import logoImage from '../Assets/PFS2.png';;
-
-
-
-
+import logoImage from '../Assets/PFS2.png';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
-
-   
 
     const handleNav = () => {
         setNav(!nav);
     };
 
     return (
-          
-        <div className="  flex justify-between items-center bg-opacity-90 h-24 max-w-[1240px] mx-auto px-4 text-[#023e8a] ">
-            
-                <img className="w-[30px] mx-4 my-4 flex items-center cursor-pointer" src={logoImage} alt="Logo"  />
-              
-            <ul className="hidden md:flex  cursor-pointer">
-              
+        <div className="flex justify-between items-center bg-opacity-90 h-24 max-w-[1240px] mx-auto px-4 text-[#023e8a]">
+           <Link to="/hero">
+            <img className="w-[30px] mx-4 my-4 flex items-center cursor-pointer" src={logoImage} alt="Logo" />
+            </Link>
+            <ul className="hidden md:flex cursor-pointer">
+                
                 <li className="p-4 hover:text-[#ffd60a]">
-                    About Us
+                    <Link to="/services">Services</Link>
                 </li>
                 <li className="p-4 hover:text-[#ffd60a]">
-                   Services
+                    <Link to="/prices">Pricing</Link>
                 </li>
                 <li className="p-4 hover:text-[#ffd60a]">
-                    Pricing
-                </li>
-                <li className="p-4 hover:text-[#ffd60a]">
-                      Contact
+                    <Link to="/contact">Contact</Link>
                 </li>
             </ul>
             <div onClick={handleNav} className="block md:hidden">
                 {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
-       
             <div
                 className={
                     nav
                         ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#f5ebe0] bg-opacity-10 ease-in-out duration-500'
-                        : 'fixed left-[-100%] '
+                        : 'fixed left-[-100%]'
                 }
             >
-              
-               
                 <ul className="uppercase p-4 cursor-pointer">
-                    
+                   
                     <li className="p-4 border-b border-gray-600 hover:text-[#ffd60a]">
-                        About Us
+                        <Link to="/services">Services</Link>
                     </li>
                     <li className="p-4 border-b border-gray-600 hover:text-[#ffd60a]">
-                      Services
+                        <Link to="/pricing">Pricing</Link>
                     </li>
                     <li className="p-4 border-b border-gray-600 hover:text-[#ffd60a]">
-                      Pricing
-                    </li>
-                    <li className="p-4 border-b border-gray-600 hover:text-[#ffd60a]">
-                       Contact 
+                        <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
             </div>
         </div>
-    
     );
 };
 
 export default Navbar;
-
